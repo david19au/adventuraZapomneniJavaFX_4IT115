bb73319240545d5aaf3c9e7a4bce58bbb0f65c1d
david19au <david19au@gmail.com>
2022-10-05 12:19:21 +0200

    edited home.fxml to reflect changes in the program, to actually process commands, made HomeController.java to be able to show the method

--- src/main/java/cz/vse/java/luut02/adventura/adventurazapomnenijavafx/main/HomeController.java
+++ src/main/java/cz/vse/java/luut02/adventura/adventurazapomnenijavafx/main/HomeController.java
@@ -1,7 +1,10 @@
 package cz.vse.java.luut02.adventura.adventurazapomnenijavafx.main;
 
+import javafx.fxml.FXML;
+
 public class HomeController {
 
+    @FXML
     private void processCommand() {
         System.out.println("zpracovavam prikaz");
     }
