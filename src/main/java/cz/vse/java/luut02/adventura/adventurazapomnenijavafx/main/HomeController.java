a6d3958ce1995387a7a955f7ac4d49dbb8e290db
david19au <david19au@gmail.com>
2022-10-12 11:35:40 +0200

    JavaFX commits - console UI, set what is an input and output, set ending

--- src/main/java/cz/vse/java/luut02/adventura/adventurazapomnenijavafx/main/HomeController.java
+++ src/main/java/cz/vse/java/luut02/adventura/adventurazapomnenijavafx/main/HomeController.java
@@ -1,11 +1,41 @@
 package cz.vse.java.luut02.adventura.adventurazapomnenijavafx.main;
 
+import cz.vse.java.luut02.adventura.adventurazapomnenijavafx.logika.Hra;
+import cz.vse.java.luut02.adventura.adventurazapomnenijavafx.logika.IHra;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HomeController {
 
     @FXML
-    private void processCommand() {
-        System.out.println("zpracovavam prikaz");
+    private TextArea vystup;
+
+    @FXML
+    private TextField vstup;
+
+    private IHra hra = new Hra();
+
+    @FXML
+    private void initialize() {
+        vystup.appendText(hra.vratUvitani()+"\n\n");
+        Platform.runLater(() -> vstup.requestFocus());
     }
+
+    @FXML
+    private void processCommand() {
+        String prikaz = vstup.getText();
+        vstup.clear();
+        vystup.appendText("> "+prikaz+"\n");
+
+        String vysledek = hra.zpracujPrikaz(prikaz);
+        vystup.appendText(vysledek+"\n\n");
+
+        if (hra.konecHry()) {
+            vystup.appendText(hra.vratEpilog());
+            vstup.setDisable(true);
+        }
+    }
+
 }
