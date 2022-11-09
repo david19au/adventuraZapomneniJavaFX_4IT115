d468a727c398555ba3b7a666f5b9dad3373474a8
david19au <david19au@gmail.com>
2022-11-09 13:34:16 +0100

    přidán příkaz inventář pro vypsání co má hráč v inventáři

---
+++ src/main/java/cz/vse/java/luut02/adventura/adventurazapomnenijavafx/logika/PrikazInventar.java
@@ -0,0 +1,22 @@
+package cz.vse.java.luut02.adventura.adventurazapomnenijavafx.logika;
+
+public class PrikazInventar implements IPrikaz{
+
+    private  static final String NAZEV = "inventar";
+    private Inventar inventar;
+
+    public PrikazInventar(Inventar inventar) {
+        this.inventar = inventar;
+    }
+
+
+    @Override
+    public String provedPrikaz(String... parametry) {
+        return inventar.nazvyVeci();
+    }
+
+    @Override
+    public String getNazev() {
+        return NAZEV;
+    }
+}
