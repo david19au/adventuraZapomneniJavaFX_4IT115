93c7409e1ac09d9426e11ec99a116381f1542a7f
david19au <david19au@gmail.com>
2022-11-10 19:54:40 +0100

    přidání dependancy javafx.web

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module cz.vse.java.luut02.adventura.adventurazapomnenijavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
 
-    opens cz.vse.java.luut02.adventura.adventurazapomnenijavafx.main to javafx.fxml;
+    opens cz.vse.java.luut02.adventura.adventurazapomnenijavafx.main to javafx.fxml, javafx.web;
     exports cz.vse.java.luut02.adventura.adventurazapomnenijavafx.main;
 }
