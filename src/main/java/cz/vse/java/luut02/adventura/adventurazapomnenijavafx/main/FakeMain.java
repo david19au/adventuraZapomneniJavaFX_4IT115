91e1230ca804e5f226416c0c07bae7fa2782b8cb
david19au <david19au@gmail.com>
2022-11-11 10:24:19 +0100

    FakeMain, jestli mi to solvene JavaFX dependencies problém

---
+++ src/main/java/cz/vse/java/luut02/adventura/adventurazapomnenijavafx/main/FakeMain.java
@@ -0,0 +1,9 @@
+package cz.vse.java.luut02.adventura.adventurazapomnenijavafx.main;
+
+import java.io.IOException;
+
+public class FakeMain {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
